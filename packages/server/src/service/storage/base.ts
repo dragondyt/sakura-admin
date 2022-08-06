85c5f384e563b218621ac9be73e5afd26871f4bd
dragondyt <20499679+dragondyt@users.noreply.github.com>
2022-08-06 13:57:31 +0800

    feat: 26

--- packages/server/src/service/storage/base.ts
+++ packages/server/src/service/storage/base.ts
@@ -1,12 +1,20 @@
-export default abstract class Base<M = any, T = any > extends think.Service {
+interface SelectOption {
+    desc?: string;
+    limit?: number;
+    offset?: number;
+    field?: string[];
+}
+
+export default abstract class Base<M = any, T = any> extends think.Service {
     tableName: string;
     mongo: (tableName: string) => M;
+
     protected constructor(tableName: string) {
         super();
         this.tableName = tableName;
     }
 
-    public abstract select(where: any, {}?: any): Promise<T>;
+    public abstract select(where: any, {desc, limit, offset, field}?: SelectOption): Promise<T>;
 
     public abstract add(entity: T, options?: any): Promise<T>;
 
--- packages/server/src/service/storage/mysql.ts
+++ packages/server/src/service/storage/mysql.ts
@@ -1,17 +1,77 @@
 import Base from "./base";
 
 export default class Mysql extends Base<any, any> {
-    add(entity: any, options?: any): Promise<any> {
-        throw new Error("Method not implemented.");
+    async add(data: any, options?: any): Promise<any> {
+        if (data.objectId) {
+            data.id = data.objectId;
+            delete data.objectId;
+        }
+        const instance = this.model(this.tableName);
+        const id = await instance.add(data);
+
+        return {...data, objectId: id};
     }
 
-    select(where: any, {}: any): Promise<any> {
+    async select(where: any, {desc, limit, offset, field}: any = {}): Promise<any> {
         const instance = this.model(this.tableName);
-        return Promise.resolve(undefined);
+        instance.where(this.parseWhere(where));
+        if (desc) {
+            instance.order(`${desc} DESC`);
+        }
+        if (limit || offset) {
+            instance.limit(offset || 0, limit);
+        }
+        if (field) {
+            field.push('id');
+            instance.field(field);
+        }
+
+        const data = await instance.select();
+
+        // @ts-ignore
+        return data.map(({id, ...cmt}) => ({...cmt, objectId: id}));
     }
 
     update(entity: any, where: Record<string, any> | undefined): Promise<any> {
         return Promise.resolve(undefined);
     }
 
+    private parseWhere(filter: Record<string, any>) {
+        const where: Record<string, any> = {};
+
+        if (think.isEmpty(filter)) {
+            return where;
+        }
+
+        // tslint:disable-next-line:forin
+        for (const k in filter) {
+            if (k === 'objectId') {
+                where.id = filter[k];
+                continue;
+            }
+
+            if (k === '_complex') {
+                where[k] = this.parseWhere(filter[k]);
+                continue;
+            }
+
+            if (filter[k] === undefined) {
+                where[k] = null;
+                continue;
+            }
+
+            if (Array.isArray(filter[k])) {
+                if (filter[k][0] === 'IN' && !filter[k][1].length) {
+                    continue;
+                }
+                if (think.isDate(filter[k][1])) {
+                    filter[k][1] = think.datetime(filter[k][1]);
+                }
+            }
+
+            where[k] = filter[k];
+        }
+
+        return where;
+    }
 }
